413a0bae0ccf85d2eb75e72488ec03f8f9249aa0
andreaspohl <andreas.g.pohl@gmail.com>
2016-10-24 18:37:05 +0200

    copies now 'done' files from 1_in to 2_stitched

--- AVRecorderSwift/FileHandler.swift
+++ AVRecorderSwift/FileHandler.swift
@@ -12,14 +12,45 @@ import Foundation
 
 class FileHandler: NSObject {
     
+    var isRunning = true
+    
     func run () {
         
         print("FileHandler entered")
         
-        // put everything else in a low prio queue
-        for i in 1...10 {
-            print(i)
-            sleep(2)
+        let fileManager = FileManager()
+        
+        //get path to movies directory
+        let moviesPath : NSString = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.moviesDirectory, FileManager.SearchPathDomainMask.allDomainsMask, true).first! as NSString
+        
+        let fromPath : NSString = moviesPath.appendingPathComponent("1_in/") as NSString
+        let toPath : NSString = moviesPath.appendingPathComponent("2_stitched/") as NSString
+        
+        while isRunning {
+            let files = try! fileManager.contentsOfDirectory(atPath: fromPath as String)
+            
+            print("\nchecking 1_in... ")
+            
+            for file in files {
+                if file.contains("done.mov") {
+                    debugPrint("moving file from 1_in to 2_stitched: ", file)
+                    
+                    let fromPathFileNameExtension = fromPath.appendingPathComponent("\(file)")
+
+                    let toPathFileNameExtension = toPath.appendingPathComponent(file)
+                    
+                    let fromURL : URL = URL(fileURLWithPath: fromPathFileNameExtension as String)
+                    let toURL : URL = URL(fileURLWithPath: toPathFileNameExtension as String)
+                    
+                    do {
+                        try fileManager.moveItem(at: fromURL, to: toURL)
+                    } catch let moveError as NSError {
+                        print(moveError.localizedDescription)
+                    }
+                }
+            }
+            
+            sleep(10)
         }
     }
 }
