4cd86382e644fde46f9bedcdb48792b4d28b254b
Andreas Pohl <andreas.g.pohl@gmail.com>
2020-01-01 17:48:59 +0100

    feat: unit test able to access MotionWrapper

---
+++ AVRecorderSwift/MotionTest-Bridging-Header.h
@@ -0,0 +1,15 @@
+
+//
+//  MortionTest-Bridging-Header.h
+//  AVRecorderSwift
+//
+//  Created by Andreas Pohl on 1.1.2020.
+//  Copyright © 2020 Andreas Pohl. All rights reserved.
+//
+
+#ifndef MotionTest_Bridging_Header_h
+#define MotionTest_Bridging_Header_h
+
+#import "MotionWrapper.h"
+
+#endif /* MotionTest_Bridging_Header_h */
